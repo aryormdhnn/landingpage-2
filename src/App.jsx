68c6b963cc61e3dcb5b6df6d5b985e2adfb7bae4
Aryo Romadhon <yoomadhon@gmail.com>
2023-07-22 15:33:30 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Feature from './components/Feature.jsx';
 import Category from './components/Category.jsx';
 import Product from './components/Product.jsx';
 import Brand from './components/Brand.jsx';
-import Cta from './components/CTA.jsx';
+import CTA from './components/Cta.jsx';
 import Footer from './components/Footer.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -21,7 +21,7 @@ function App() {
       <Category />
       <Product />
       <Brand />
-      <Cta />
+      <CTA />
       <Footer />
     </>
   )
