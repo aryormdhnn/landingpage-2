db8951b3928306f55a8890225e998023e81ed1dc
Aryo Romadhon <yoomadhon@gmail.com>
2023-07-22 15:26:25 +0700

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-
 import React from 'react'
 import Header from './components/Header.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './components/Hero.jsx';
 import Feature from './components/Feature.jsx';
 import Category from './components/Category.jsx';
